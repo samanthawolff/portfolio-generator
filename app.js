702c8973e2b2cbd26f4762abc712b89191a3ccf8
Samantha Wolff <samanthawolff07@gmail.com>
2022-03-31 00:11:26 -0500

    handling node.js asynchronicity

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template.js');
+const { writeFile, copyFile} = require('./utils/generate-site.js');
 
 
 const promptUser = () => {
@@ -133,17 +133,51 @@ const promptProject = portfolioData => {
     });
 };
 
+
 promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+
+
+
+/* promptUser()
     .then(promptProject)
     .then(portfolioData => {
         const pageHTML = generatePage(portfolioData);
 
-        fs.writeFile('./index.html', pageHTML, err => {
-            if (err) throw err;
-
+        fs.writeFile('./dist/index.html', pageHTML, err => {
+            if (err) {
+                console.log(err);
+                return;
+            }
             console.log('Page created! Check out index.html in this directory to see it!');
+
+            fs.copyFile('./src/style.css', './dist/style.css', err => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log('Style sheet copied successfully!');
+            });
     });
-});
+}); */
 
 
 
---
+++ utils/generate-site.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./dist/index.html', fileContent, err => {
+            // if there is an error, reject the Promise and sednd the error to the Promise's `.catch()` method
+            if (err) {
+                reject(err);
+                // return out of the function here to make sure the promise doesnt accidentally execute the resolve() function as well
+                return;
+            }
+
+            // if everything went well, resolve the promise and send the successful data to the `.then()` method
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });    
+    });
+};
+
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+    
+            resolve({
+                ok: true,
+                message: 'Stylesheet created!'
+            });
+        });
+   });
+};
+
+
+module.exports = {
+    writeFile: writeFile,
+    copyFile: copyFile
+};
